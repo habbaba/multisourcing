2745116f0fdc8479021ada901864027a98086303
habbaba <34061969+habbaba@users.noreply.github.com>
2025-04-05 12:46:18 +0300

    Delete website_sale_multi_warehouse directory

--- website_sale_multi_warehouse/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# models/__init__.py
-from . import stock_warehouse
-from . import sale_order
-from . import res_config_settings
-from . import procurement_group
-from . import stock_picking
-from . import website
